968adf3e2bd88a7621ac99211df063daf067e53b
axhindol <axhindol@umich.edu>
2017-05-25 18:33:30 -0700

    lets-encrypt

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from flask import *
 from flask_bootstrap import Bootstrap
-from flask_letsencrypt import LetsEncrypt
-
-responses = {
-        'foo': 'bar',
-        'baz': 'gak',
-    }
-
-def handle_letsencrypt_challenge(challenge):
-    return responses.get(challenge, None)
 
 app = Flask('app')
-le = LetsEncrypt(app)
-le.challenge_loader(handle_letsencrypt_challenge)
-
 app = Flask(__name__)
 Bootstrap(app)
 '''
 $ export FLASK_APP=main.py
 $ python -m flask run
 '''
+
+
 @app.route('/')
 def main_led_route():
     return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
+
+
+@app.route('/.well-known/acme-challenge/<token_value>')
+def letsencrpyt(tmp):
+    with open('.well-known/acme-challenge/{}'.format(token_value)) as f:
+        answer = f.readline().strip()
+    return answer
