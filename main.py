3b9e7a2a531ab4fcc5a5b44d42730fadf5b2e168
Arber Xhindoli <axhindol@umich.edu>
2017-05-26 20:04:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+Authored By: Arber Xhindoli github:@arberx
+Code is free for anyone to use
+'''
+
 from flask import *
 from flask_bootstrap import Bootstrap
 
@@ -9,15 +14,16 @@ $ export FLASK_APP=main.py
 $ python -m flask run
 '''
 
-
+'''
+Main route, serves index.html
+'''
 @app.route('/')
 def main_led_route():
     return render_template('index.html')
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0')
-
-
+'''
+Need for letsencrypt server challenge
+'''
 @app.route('/.well-known/acme-challenge/<token_value>')
 def letsencrpyt(tmp):
     with open('.well-known/acme-challenge/{}'.format(token_value)) as f:
