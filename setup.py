8f5f4d9abed1ca8395a7a78c96dc83b959328b76
arberx <axhindol@umich.edu>
2019-02-02 21:28:52 +0000

    structural changes

---
+++ setup.py
@@ -0,0 +1,19 @@
+###
+# arberweb
+# Arber Xhindoli github:@arberx
+##
+
+from setuptools import setup
+
+setup(
+    name='arberweb',
+    version='0.1',
+    # packages=['arberweb'],
+    # include_package_data=True,
+    install_requires=[
+        'Flask',
+        'pylint',
+        'flask_bootstrap',
+        'nodeenv'
+    ]
+)
