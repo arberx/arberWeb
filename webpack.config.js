598cf00b4327e26253c517033e61daec26cd2615
arberx <axhindol@umich.edu>
2019-02-05 22:02:23 -0500

    add react/webpack/babel dependencies

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+const config = {
+    entry:  __dirname + '/static/js/tutor.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+          }
+        ]
+    }
+};
+
+module.exports = config;
