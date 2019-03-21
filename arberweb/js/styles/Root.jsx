dfdeb1ced9d36cabc0256195e2db0d02e8160bd2
arberx <axhindol@umich.edu>
2019-03-20 20:40:09 -0400

    I hate css

--- arberweb/js/styles/Root.jsx
+++ arberweb/js/styles/Root.jsx
@@ -9,13 +9,12 @@ import styled from 'styled-components';
     Wraps all other elements, sets defaults.
 */
 const Root = styled.div`
-    min-height: 100vh;
     display: grid;
     grid-template-rows: 1fr auto;
     grid-template-columns: 100%;
     line-height: 1.3;
     background-color: #f6faff;
-    font-family: Calibre, San Francisco, SF Pro Text, -apple-system, system-ui, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif;
+    font-family: Source Sans Pro,sans-serif;
+    font-size: 16px;
 `
-
 export default Root;
