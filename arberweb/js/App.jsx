02ff98fd156775d7458234c7c85c16999fc8fc9b
arberx <axhindol@umich.edu>
2019-03-20 20:40:09 -0400

    I hate css

--- arberweb/js/App.jsx
+++ arberweb/js/App.jsx
@@ -4,23 +4,29 @@ Arber Xhindoli github:@arberx
 */
 
 import React from "react";
+
 import Root from "./styles/Root";
 import Header from "./styles/Header";
+import styled from 'styled-components';
 import Content from "./styles/Content";
 import Section from "./styles/Section";
 import { H1, H2, H3 }  from "./styles/BasicElements";
 
+const NameDiv = styled.div`
+     flex-grow: 1;
+     font-size: 16px;
+`;
 export default class App extends React.Component {
   render () {
     return(
-      <Root>
-        <Header />
-          <Content>
-            <Section>
-              {/* Intro */}
-            </Section>
-          </Content>
-      </Root>
+      // <Root>
+        <Header>
+          <NameDiv>
+              Arber X
+          </NameDiv>
+        </Header>
+      // </Root>
     )
   }
+
 }
--- arberweb/js/styles/Header.jsx
+++ arberweb/js/styles/Header.jsx
@@ -6,16 +6,20 @@ Arber Xhindoli github:@arberx
 import styled from 'styled-components';
 
 const Header = styled.div`
+    position: absolute;
+    z-index: 20;
+    top: 0;
+    left: 0;
+    right: 0;
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    position: fixed;
-    top: 0px;
-    padding: 0px 50px;
+    margin-top: 88px;
+    max-width: 1140px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0 30px;
     background-color: #f6faff;
-    width: 100%;
-    height: 70px;
-    box-shadow: rgba(2, 12, 27, 0.9) 0px 2px 4px;
+    /* box-shadow: rgba(2, 12, 27, 0.9) 0px 2px 4px; */
 `;
 
 export default Header;
