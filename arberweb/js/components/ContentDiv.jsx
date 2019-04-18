3bc8e4cfe8143e283573d823d950dbe024ce0d16
Arber Xhindoli <axhindoli@bloomberg.net>
2019-04-18 17:45:35 -0400

    format using prettier

--- arberweb/js/components/ContentDiv.jsx
+++ arberweb/js/components/ContentDiv.jsx
@@ -3,32 +3,34 @@ arberweb
 Arber Xhindoli github:@arberx
 */
 
-import styled from 'styled-components';
+import styled from "styled-components";
 // local includes
-import { device } from './MediaBreak';
+import { device } from "./MediaBreak";
 
 /*
     This component wraps content using RootGrid as a template.
 */
 const ContentDiv = styled.div`
-    grid-column: rside / lside;
-    grid-row: ${props => props.row || "third-line / fourth-line"};
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-        margin-top: 190px;
-	};
-	 @media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-        margin-top: 215px;
-	};
-    max-width: 1140px;
-    margin-left: auto;
-    margin-right: auto;
-    padding-left: 10%;
-    text-align: left;
-    line-height: 3.4em;
+	grid-column: rside / lside;
+	grid-row: ${props => props.row || "third-line / fourth-line"};
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+		margin-top: 190px;
+	}
+	@media ${device.laptop},
+		${device.tablet},
+		${device.desktop},
+		${device.desktopL} {
+		margin-top: 215px;
+	}
+	max-width: 1140px;
+	margin-left: auto;
+	margin-right: auto;
+	padding-left: 10%;
+	text-align: left;
+	line-height: 3.4em;
 `;
 
 export default ContentDiv;
-
--- arberweb/js/components/Form.jsx
+++ arberweb/js/components/Form.jsx
@@ -4,8 +4,8 @@ Arber Xhindoli github:@arberx
 */
 
 import React from "react";
-import styled from 'styled-components';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import styled from "styled-components";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 
 const FormDiv = styled.div`
 	grid-column: line1 / line2;
@@ -13,32 +13,42 @@ const FormDiv = styled.div`
 `;
 
 export default class FormContainer extends React.Component {
-	render () {
-	  return(
-		<FormDiv>
+	render() {
+		return (
+			<FormDiv>
 				<Formik
 					initialValues={"arber" /** { email, social } */}
 					onSubmit={(values, actions) => {
-						console.log("submit form!")
+						console.log("submit form!");
 					}}
 					render={({ errors, status, touched, isSubmitting }) => (
-					<Form>
-						<Field type="email" name="email" />
-						<ErrorMessage name="email" component="div" />  
-						<Field type="text" className="error" name="social.facebook" />
-						<ErrorMessage name="social.facebook">
-						{errorMessage => <div className="error">{errorMessage}</div>}
-						</ErrorMessage>
-						<Field type="text" name="social.twitter" />
-						<ErrorMessage name="social.twitter" className="error" component="div"/>  
-						{status && status.msg && <div>{status.msg}</div>}
-						<button type="submit" disabled={isSubmitting}>
-						Submit
-						</button>
-					</Form>
+						<Form>
+							<Field type="email" name="email" />
+							<ErrorMessage name="email" component="div" />
+							<Field
+								type="text"
+								className="error"
+								name="social.facebook"
+							/>
+							<ErrorMessage name="social.facebook">
+								{errorMessage => (
+									<div className="error">{errorMessage}</div>
+								)}
+							</ErrorMessage>
+							<Field type="text" name="social.twitter" />
+							<ErrorMessage
+								name="social.twitter"
+								className="error"
+								component="div"
+							/>
+							{status && status.msg && <div>{status.msg}</div>}
+							<button type="submit" disabled={isSubmitting}>
+								Submit
+							</button>
+						</Form>
 					)}
 				/>
-		</FormDiv>
-	  )
+			</FormDiv>
+		);
 	}
-};
+}
