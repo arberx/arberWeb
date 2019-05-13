2ee20108fb5b4523de5ea7b1edc5b1e4fa2e8268
arberx <axhindol@umich.edu>
2019-05-12 20:22:38 -0400

    delete terminal picture, add ga

--- arberweb/js/components/Content.jsx
+++ arberweb/js/components/Content.jsx
@@ -12,76 +12,83 @@ import ContentDiv from "./ContentDiv";
 import { device } from "./MediaBreak";
 
 const Row1 = styled.div`
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		font-size: 25px;
-		width: 240px;
-		align-self: center;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		font-size: 30px;
-		width: 500px;
-	}
-	line-height: 1.2em;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    font-size: 25px;
+    width: 240px;
+    align-self: center;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    font-size: 30px;
+    width: 500px;
+  }
+  line-height: 1.2em;
 `;
 
 const Row2 = styled.div`
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		font-size: 40px;
-		width: 300px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		font-size: 50px;
-		width: 350px;
-	}
-	font-weight: 900;
-	display: inline-block;
-	position: absoulte;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    font-size: 40px;
+    width: 300px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    font-size: 50px;
+    width: 350px;
+  }
+  font-weight: 900;
+  display: inline-block;
 `;
 
 const Row3 = styled.div`
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		font-size: 25px;
-		width: 240px;
-		margin-top: 80px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		font-size: 30px;
-		width: 600px;
-		margin-top: 150px;
-	}
-	line-height: 1.2em;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    font-size: 25px;
+    width: 240px;
+    margin-top: 80px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    font-size: 30px;
+    width: 600px;
+  }
+  line-height: 1.2em;
 `;
 
 const subjectArray = [
-	"AP CompSci.",
-	"Java.",
-	"C++.",
-	"Python.",
-	"Web Dev.",
-	"JavaScript.",
-	"C.",
-	"Physics.",
-	"Math."
+  "AP CompSci.",
+  "Java.",
+  "C++.",
+  "Python.",
+  "Web Dev.",
+  "JavaScript.",
+  "C.",
+  "Physics.",
+  "Math."
 ];
 
 export default class Content extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<ContentDiv>
-				<Fade left>
-					<Row1>Hi, I'm Arber and I teach</Row1>
-					<Row2>
-						<TextLoop children={subjectArray} interval={3000} />
-					</Row2>
-					<Row3>
-						I'm a software engineer based in NYC, specializing in
-						<strong> Distributed Systems.</strong>
-					</Row3>
-				</Fade>
-			</ContentDiv>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ContentDiv>
+        <Fade left>
+          <Row1>Hi, I'm Arber and I teach</Row1>
+          <Row2>
+            <TextLoop children={subjectArray} interval={3000} />
+          </Row2>
+          <Row3>
+            I'm a software engineer based in NYC, specializing in
+            <strong> Distributed Systems.</strong>
+          </Row3>
+        </Fade>
+      </ContentDiv>
+    );
+  }
 }
--- arberweb/js/components/ContentDiv.jsx
+++ arberweb/js/components/ContentDiv.jsx
@@ -11,22 +11,25 @@ import { device } from "./MediaBreak";
     This component wraps content using RootGrid as a template.
 */
 const ContentDiv = styled.div`
-	grid-column: content;
-	grid-row: ${props => props.row || "third-line / fourth-line"};
-	display: flex;
-	flex-direction: column;
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		margin-top: 100px;
-		padding-left: 50px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		margin-top: 250px;
-	}
-	text-align: left;
-	line-height: 3.4em;
-	align-self: center;
-	justify-self: center;
-	justify-content: center;
+  grid-column: content;
+  grid-row: ${props => props.row || third - line / fourth - line};
+  display: flex;
+  flex-direction: column;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    margin-top: 100px;
+    padding-left: 50px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    margin-top: 250px;
+  }
+  text-align: left;
+  line-height: 3.4em;
+  align-self: center;
+  justify-self: center;
+  justify-content: center;
 `;
 
 export default ContentDiv;
