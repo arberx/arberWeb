daf36a8e6e35d343f1093e9b220c0e2b80153d2c
arberx <axhindol@umich.edu>
2019-07-06 14:52:11 -0400

    Remove tutoring stuff, WIP consulting page

--- arberweb/js/components/About.jsx
+++ arberweb/js/components/About.jsx
@@ -10,120 +10,132 @@ import Fade from "react-reveal/Fade";
 import { device } from "./MediaBreak";
 
 const AboutDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		margin-top: 100px;
-		font-size: 15px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		flex-direction: column;
-		font-size: 25px;
-		margin-top: 100px;
-	}
-	grid-row: third-line / fourth-line;
-	grid-column: content;
-	justify-content: space-evenly;
-	align-content: center;
-	align-items: center;
-	align-self: center;
-	justify-self: center;
-	line-height: 1.2em;
-	flex-wrap: wrap;
-	width: 100%;
-	height: 100%;
+  display: flex;
+  flex-direction: column;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    margin-top: 100px;
+    font-size: 15px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    flex-direction: column;
+    font-size: 25px;
+    margin-top: 100px;
+  }
+  grid-row: third-line / fourth-line;
+  grid-column: content;
+  justify-content: space-evenly;
+  align-content: center;
+  align-items: center;
+  align-self: center;
+  justify-self: center;
+  line-height: 1.2em;
+  flex-wrap: wrap;
+  /* width: 100%; */
+  /* height: 100%; */
 `;
 
 const ArberImg = styled.img`
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		width: 200px;
-		height: 200px;
-	}
-	@media ${device.laptop}, ${device.tablet} {
-		width: 250px;
-		height: 250px;
-	}
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    width: 200px;
+    height: 200px;
+  }
+  @media ${device.laptop}, ${device.tablet} {
+    width: 250px;
+    height: 250px;
+  }
 
-	@media ${device.desktop}, ${device.desktopL} {
-		width: 300px;
-		height: 300px;
-	}
-	border-radius: 50%;
+  @media ${device.desktop}, ${device.desktopL} {
+    width: 300px;
+    height: 300px;
+  }
+  border-radius: 50%;
 `;
 
 const TextDiv = styled.div`
-	display: flex;
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		flex-direction: column;
-		/* margin-top: 10px; */
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		flex-direction: column;
-		padding-left: 100px;
-	}
-	justify-content: space-between;
-	align-content: center;
-	align-self: center;
-	justify-self: center;
+  display: flex;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    flex-direction: column;
+    /* margin-top: 10px; */
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    flex-direction: column;
+    padding-left: 100px;
+  }
+  justify-content: space-between;
+  align-content: center;
+  align-self: center;
+  justify-self: center;
 `;
 
 const H1Style = styled.h1`
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		font-size: 25px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		font-size: 30px;
-	}
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    font-size: 25px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    font-size: 30px;
+  }
 `;
 const ULStyle = styled.ul`
-	list-style: circle outside none;
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		font-size: 15px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		font-size: 20px;
-	}
+  list-style: circle outside none;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    font-size: 15px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    font-size: 20px;
+  }
 `;
 
 const WIASection = styled.section``;
 const WCDSection = styled.section``;
 
 export default class About extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<AboutDiv>
-				<Fade left>
-					<ArberImg src="../../static/my_picture.jpg" alt="Arber X" />
-				</Fade>
-				<TextDiv>
-					<Fade left>
-						<H1Style> Who I am </H1Style>
-						<ULStyle>
-							<li> Full-time Software Engineer at a top tech company. </li>
-							<br />
-							<li> Graduate of a top 5 engineering program - Go Blue! </li>
-							<br />
-							<li> 5+ years of tutor/mentor experience. </li>
-						</ULStyle>
-					</Fade>
-					<Fade left>
-						<H1Style> What I do </H1Style>
-						<ULStyle>
-
-							<li> I teach people how to code! </li>
-							<br />
-							<li>
-								Provide customized learning plans and projects depending on ability.
-							</li>
-							<br />
-							<li> Teach programming at all skills and age levels. </li>
-						</ULStyle>
-					</Fade>
-				</TextDiv>
-			</AboutDiv>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <AboutDiv>
+        <Fade left>
+          <ArberImg src="../../static/my_picture.jpg" alt="Arber X" />
+        </Fade>
+        <TextDiv>
+          <Fade left>
+            <H1Style> Who I am </H1Style>
+            <ULStyle>
+              <li> Full-time Software Engineer at a top tech company. </li>
+              <br />
+              <li> Graduate of a top 5 engineering program - Go Blue! </li>
+              <br />
+              <li> 5+ years of tutor/mentor experience. </li>
+            </ULStyle>
+          </Fade>
+          <Fade left>
+            <H1Style> What I do </H1Style>
+            <ULStyle>
+              <li> I teach people how to code! </li>
+              <br />
+              <li>
+                Provide customized learning plans and projects depending on
+                ability.
+              </li>
+              <br />
+              <li> Teach programming at all skills and age levels. </li>
+            </ULStyle>
+          </Fade>
+        </TextDiv>
+      </AboutDiv>
+    );
+  }
 }
--- arberweb/js/components/Footer.jsx
+++ arberweb/js/components/Footer.jsx
@@ -9,61 +9,67 @@ import styled from "styled-components";
 import { device } from "./MediaBreak";
 
 const FooterDiv = styled.div`
-	grid-column: content;
-	grid-row: fifth-line;
-	display: flex;
-	position: absoulte;
-	justify-content: space-evenly;
-	align-self: center;
-	flex-direction: row;
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		margin-top: 50px;
-		width: 100%;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		width: 50%;
-		padding-left: 150px;
-	}
-	margin-bottom: 50px;
+  grid-column: content;
+  grid-row: fourth-line/fifth-line;
+  display: flex;
+  justify-content: space-evenly;
+  align-self: center;
+  flex-direction: row;
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    /* margin-top: 50px; */
+    width: 100%;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    /* margin-top: 100px; */
+    width: 50%;
+    padding-left: 150px;
+  }
+  margin-bottom: 50px;
 `;
 
 const ImgDiv = styled.img`
-	@media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
-		width: 40px;
-		height: 40px;
-	}
-	@media ${device.laptop}, ${device.tablet}, ${device.desktop}, ${device.desktopL} {
-		width: 50px;
-		height: 50px;
-	}
+  @media ${device.mobileS}, ${device.mobileM}, ${device.mobileL} {
+    width: 40px;
+    height: 40px;
+  }
+  @media ${device.laptop},
+    ${device.tablet},
+    ${device.desktop},
+    ${device.desktopL} {
+    width: 50px;
+    height: 50px;
+  }
 `;
 
 export default class Footer extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<FooterDiv>
-				<a href="https://www.facebook.com/arberxhindoli">
-					<ImgDiv
-						src="../../static/iconfinder_facebook_online_social_media_734399.svg"
-						alt="Arber X Facebook"
-					/>
-				</a>
-				<a href="https://www.github.com/arberx">
-					<ImgDiv
-						src="../../static/iconfinder_github_social_media_logo_1221583.svg"
-						alt="Arber X Github"
-					/>
-				</a>
-				<a href="https://www.linkedin.com/in/arberx">
-					<ImgDiv
-						src="../../static/iconfinder_online_social_media_linked_in_734383.svg"
-						alt="Arber X Linkedin"
-					/>
-				</a>
-			</FooterDiv>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <FooterDiv>
+        <a href="https://www.facebook.com/arberxhindoli">
+          <ImgDiv
+            src="../../static/iconfinder_facebook_online_social_media_734399.svg"
+            alt="Arber X Facebook"
+          />
+        </a>
+        <a href="https://www.github.com/arberx">
+          <ImgDiv
+            src="../../static/iconfinder_github_social_media_logo_1221583.svg"
+            alt="Arber X Github"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/arberx">
+          <ImgDiv
+            src="../../static/iconfinder_online_social_media_linked_in_734383.svg"
+            alt="Arber X Linkedin"
+          />
+        </a>
+      </FooterDiv>
+    );
+  }
 }
